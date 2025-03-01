a1ea33915e444228062d84c46505f827ae321a87
fjlein <fjlein@gmail.com>
2025-03-01 13:34:37 +0100

    extract songs from spotify playlists and att them to the pdf

--- hello.typ
+++ hello.typ
@@ -1,24 +1,22 @@
 #set page(
-  paper: "a4",
-  margin: 2cm
+  paper: "a4", //210 x 297
+  margin: 1.5cm
 )
 
-#let songs = json.decode(sys.inputs.data)
-
-
+#let songs = json("response.json")
 
 #let back(song) = {
   box(
-    height: 7.5cm,
-    width: 7.5cm,
-    fill: gray,
+    height: 5cm,
+    width: 5cm,
+    stroke: gray,
     [#song.name]
   )
 }
 
 #grid(
-  columns: 2,
-  column-gutter: 2cm,
+  columns: 3,
+  column-gutter: 1.5cm,
   row-gutter: 1.6cm,
   ..for song in songs {
     (back(song),)
--- main.py
+++ main.py
@@ -1,117 +1,64 @@
-# This is a sample Python script.
 import json
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import spotipy
 import typst
+from spotipy import SpotifyClientCredentials
 
-songs = [
-    {
-        "name": "Das Geht Ab (Wir Feiern Die Ganze Nacht) - Real Booty Babes Edit",
-        "url": "https://open.spotify.com/track/3YdyHxOzSMglzwI9qMpuc0",
-        "year": 2009,
-        "artists": [
-            "Frauenarzt",
-            "Manny Marc"
-        ]
-    },
-    {
-        "name": "Play Hard (feat. Ne-Yo & Akon)",
-        "url": "https://open.spotify.com/track/5YPMEOJ58kfl56VHxTgwx3",
-        "year": 2012,
-        "artists": [
-            "David Guetta",
-            "Ne-Yo",
-            "Akon"
-        ]
-    },
-    {
-        "name": "Disco Pogo - Atzen Musik Mix",
-        "url": "https://open.spotify.com/track/66sA9JeAeRuN1ZMtvthPQM",
-        "year": 2010,
-        "artists": [
-            "Die Atzen"
-        ]
-    },
-    {
-        "name": "Sweat - Snoop Dogg vs David Guetta Remix",
-        "url": "https://open.spotify.com/track/0R7YVi7w41Dr9jU5vblAok",
-        "year": 2011,
-        "artists": [
-            "Snoop Dogg",
-            "David Guetta"
-        ]
-    },
-    {
-        "name": "Sexy And I Know It",
-        "url": "https://open.spotify.com/track/70Vdd1gx5tn84jkAU31ASv",
-        "year": 2011,
-        "artists": [
-            "LMFAO"
-        ]
-    },
-    {
-        "name": "Call Me Maybe",
-        "url": "https://open.spotify.com/track/20I6sIOMTCkB6w7ryavxtO",
-        "year": 2012,
-        "artists": [
-            "Carly Rae Jepsen"
-        ]
-    },
-    {
-        "name": "I Gotta Feeling",
-        "url": "https://open.spotify.com/track/70cTMpcgWMcR18t9MRJFjB",
-        "year": 2009,
-        "artists": [
-            "Black Eyed Peas"
-        ]
-    },
-    {
-        "name": "Would I Lie to You - Radio Edit",
-        "url": "https://open.spotify.com/track/4PdJSsESm34djLfBde9Pr2",
-        "year": 2016,
-        "artists": [
-            "David Guetta",
-            "Cedric Gervais",
-            "Chris Willis"
-        ]
-    },
-    {
-        "name": "Don't Be So Shy - Filatov & Karas Remix",
-        "url": "https://open.spotify.com/track/5Sod5qQTp8Nj4n5dTjttQG",
-        "year": 2015,
-        "artists": [
-            "Imany",
-            "Filatov & Karas"
-        ]
-    },
-    {
-        "name": "Sorry",
-        "url": "https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-        "year": 2015,
-        "artists": [
-            "Justin Bieber"
-        ]
-    },
-    {
-        "name": "Danza Kuduro",
-        "url": "https://open.spotify.com/track/2a1o6ZejUi8U3wzzOtCOYw",
-        "year": 2010,
-        "artists": [
-            "Don Omar",
-            "Lucenzo"
-        ]
-    }
-]
+
+def get_env_var(key):
+    value = os.getenv(key)
+    if not value:
+        raise EnvironmentError(f"Environment variable {key} is required but not set.")
+    return value
+
+
+def format_german_date(date_str):
+    date_parts = date_str.split("-")
+
+    if len(date_parts) == 3:
+        return f"{date_parts[2]}.{date_parts[1]}.{date_parts[0]}"
+    elif len(date_parts) == 2:
+        return f"{date_parts[1]}.{date_parts[0]}"
+    elif len(date_parts) == 1:
+        return date_parts[0]
+    return date_str
+
+
+def get_playlist_tracks(sp, playlist_id):
+    tracks = []
+    results = sp.playlist_tracks(playlist_id)
+
+    while results:
+        for item in results["items"]:
+            track = item["track"]
+            if track:
+                tracks.append({
+                    "name": track["name"],
+                    "artist": ", ".join([artist["name"] for artist in track["artists"]]),
+                    "release_date": format_german_date(track["album"]["release_date"]),
+                    "url": track["external_urls"]["spotify"]
+                })
+
+        results = sp.next(results) if results["next"] else None
+
+    return tracks
 
 
 def main():
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyClientCredentials(
+            client_id=get_env_var("CLIENT_ID"),
+            client_secret=get_env_var("CLIENT_SECRET"),
+        )
+    )
 
-    data = json.dumps(songs)
-    sys_inputs: dict[str, str] = {"data": data}
+    songs = get_playlist_tracks(sp, get_env_var("PLAYLIST_ID"))
 
-    typst.compile("hello.typ", output="hello.pdf", sys_inputs=sys_inputs)
+    with open("response.json", "w") as file:
+        json.dump(songs, file, indent=4)
+
+    typst.compile("hello.typ", output="hello.pdf")
 
 
 # Press the green button in the gutter to run the script.
